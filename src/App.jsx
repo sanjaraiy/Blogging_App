418d87b897b2f6c70cf2a7694ac85e2e8cd47ba4
sanjaraiy <deepakrai78788@gmail.com>
2024-01-29 19:00:43 +0530

    added config file and integrated with appwrite BAAS

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 
 function App() {
+  // console.log(process.env.REACT_APP_APPWRITE_URL);
+  console.log(import.meta.env.VITE_APPWRITE_URL);
   return (
     <>
       <h1>Blog in AppWrite</h1>
